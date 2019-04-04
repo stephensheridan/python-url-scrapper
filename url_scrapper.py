6071f6fcac499ee1361371735edc9ec74bfce6d6
Stephen Sheridan <stesher@gmail.com>
2019-04-04 19:38:23 +0100

    Changed data structure to set to store unique urls. Also added command line arguments to make running script more flexible. Added check to only store urls that start with http.

--- url_scrapper.py
+++ url_scrapper.py
@@ -1,49 +1,60 @@
+#!/usr/bin/python
+
+import sys
 import subprocess
 
 # Example of command line used 
-# curl -L www.yahoo.com | grep -oE 'href=\"(\S*)'
+# curl -L --max-time 10 www.yahoo.com | grep -oE 'href=\"(\S*)'
 
-def getURLS(page):
-    # curl and follow redirect
-    command  = "curl -L "
-    # Pull out string that start with href=" followed by repeating non-whitespace
-    grep_filter = "| grep -oE 'href=\"(\S*)'"
-    # Call the curl and pipe the output to grep
-    url_data = subprocess.Popen(command + " " + page + " " + grep_filter, shell=True, stdout=subprocess.PIPE).stdout.read()
-    # Split the output based on carriage returns (each line of output from grep)
-    url_data = url_data.strip().split('\n')
-    urls = []
-    for line in url_data:
-        if ("http" in line):
-            left = line.find("\"")
-            right = line.rfind("\"")
-            s = line[left+1:right]
-            if (len(s) > 0):
-                urls.append(line[left+1:right])
-    return urls
+INPUT_FILE = "./data/alexa_100k.csv"
+OUTPUT_FILE = "./data/urls.csv"
+
+def getURLS(url_set, page):
+  # curl and follow redirect
+  command  = "curl -L --max-time 10"
+  # Pull out string that start with href=" followed by repeating non-whitespace
+  grep_filter = "| grep -oE 'href=\"(\S*)'"
+  # Call the curl and pipe the output to grep
+  url_data = subprocess.Popen(command + " " + page + " " + grep_filter, shell=True, stdout=subprocess.PIPE).stdout.read()
+  # Split the output based on carriage returns (each line of output from grep)
+  url_data = url_data.strip().split('\n')
+  for line in url_data:
+      left = line.find("\"")
+      right = line.rfind("\"")
+      s = line[left+1:right]
+      if ("http" in s[0:4]):
+        url_set.add(line[left+1:right])
 
 
-# Number of line to read from Alexa top 100K
-LINES_TO_READ = 50
-# Path to Alexa csv file
-fin = open("./data/alexa_100k.csv", "r")
-# Path to URL file we will generate
-fout = open("./data/urls.csv","w+")
-# Count of urls output
-line_count = 1
-for i in range(LINES_TO_READ):
-  # Strip the whitespace and separate by ',' and take just the domain part of Alexa entry
-  s = fin.readline().strip().split(',')[1]
-  # Crawl the page and pull out list of URLS
-  urls = getURLS(s)
+def main(input_file, output_file, num_pages_to_scrape):
+  # Open Alexa csv file
+  fin = open(input_file, "r")
+  # Open output file for writing
+  fout = open(output_file,"w+")
+  # Create a set to contain unique Url's
+  unique_urls = set()
+  # Count of urls output
+  line_count = 0
+  for i in range(num_pages_to_scrape):
+    # Strip the whitespace and separate by ',' and take just the domain part of Alexa entry
+    s = fin.readline().strip().split(',')[1]
+    # Crawl the page and pull out list of URLS
+    getURLS(unique_urls, s)
+
   # Write these URLS to our new URL output file
-  for u in urls:
+  for u in unique_urls:
     fout.write(str(line_count) + "," + u + "\r\n")
     line_count = line_count + 1
     
-# Close the files
-fin.close()
-fout.close()
+  # Close the files
+  fin.close()
+  fout.close()
+  print "Done, " + str(line_count) + " URL's scrapped from  " + str(num_pages_to_scrape) + " pages."
 
+if __name__ == "__main__":
+    if (len(sys.argv) < 4):
+      print "USAGE: url_scrapper <input_file> <output_file> <num_pages_to_scrape>"
+    elif (len(sys.argv) == 4):
+      main(sys.argv[1], sys.argv[2], int(sys.argv[3]))
 
 
